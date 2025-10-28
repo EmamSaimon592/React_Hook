ac7e703a4fcd258c4f40d8b8ae4e16ae8983dab2
EmamSaimon592 <likonsaimon123@gmail.com>
2025-10-28 21:57:06 +0600

    Some modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [color, setColor] = useState('green');
+ 
+  const changeColor = () => {
+    setColor('Green')
+  };
+
   return (
     <div>
-      <h1>Imam Hossain</h1>
+      <h1>My favourite Color is {color}</h1>
+      <button onClick={changeColor}>Blue</button>
     </div>
   );
 };
